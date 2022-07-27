967b4a233059e9db1402583b6013e3ffbdfed7bf
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-07-27 12:21:34 +0530

    feat: Modular build system
    This commit adds support for a modular build system. What is that?
    
    What if you only care about non-root variant of both apps?
    What if you just want to compile the root variant of YT? or non-root?
    
    While using the CLI manually, it allows the user to choose which app they want
    to build. You can choose to compile root or non-root variant of YT or YTM etc
    However, with the script you would have to wait for both builds to compile.
    Not anymore though.
    
    You would have to wait for both of the build to compile. It was a waste of time
    and resources. Now, not only will your build times be faster but you will
    have control over particular app & its variant.

--- build_revanced.sh
+++ build_revanced.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-# File containing all patches
-patch_file=./patches.txt
+# Configuration file
+CONFIG=./.config
+
+# source can read from process substitution, hence, it only imports the content provided by head i.e the first 3 lines
+# We do this because config file not only contains variables but also patch names & sourcing a file in bash executes it.
+source <( head -n 3 "$config" )
 
 # Get line numbers where included & excluded patches start from. 
 # We rely on the hardcoded messages to get the line numbers using grep
-excluded_start="$(grep -n -m1 'EXCLUDE PATCHES' "$patch_file" | cut -d':' -f1)"
-included_start="$(grep -n -m1 'INCLUDE PATCHES' "$patch_file" | cut -d':' -f1)"
+excluded_start="$(grep -n -m1 'EXCLUDE PATCHES' "$CONFIG" | cut -d':' -f1)"
+included_start="$(grep -n -m1 'INCLUDE PATCHES' "$CONFIG" | cut -d':' -f1)"
 
 # Get everything but hashes from between the EXCLUDE PATCH & INCLUDE PATCH line
 # Note: '^[^#[:blank:]]' ignores starting hashes and/or blank characters i.e, whitespace & tab excluding newline
-excluded_patches="$(tail -n +$excluded_start $patch_file | head -n "$(( included_start - excluded_start ))" | grep '^[^#[:blank:]]')"
+excluded_patches="$(tail -n +$excluded_start $CONFIG | head -n "$(( included_start - excluded_start ))" | grep '^[^#[:blank:]]')"
 
 # Get everything but hashes starting from INCLUDE PATCH line until EOF
-included_patches="$(tail -n +$included_start $patch_file | grep '^[^#[:blank:]]')"
+included_patches="$(tail -n +$included_start $CONFIG | grep '^[^#[:blank:]]')"
 
 # Array for storing patches
 declare -a patches
@@ -43,6 +47,17 @@ populate_patches() {
     while read -r patch; do
         patches+=("$1 $patch")
     done <<< "$2"
+}
+
+# Function containing ReVanced build command
+build_apps() {
+    echo "--------------------------------"
+    echo "Building ${5:9}"
+    echo "--------------------------------"
+    java -jar revanced-cli.jar $1 -b revanced-patches.jar $2 \ 
+                $3 ${patches[@]} \
+                $EXPERIMENTAL \
+                $4 $5
 }
 
 ## Main
@@ -82,41 +97,59 @@ fi
 [[ ! -z "$excluded_patches" ]] && populate_patches "-e" "$excluded_patches"
 [[ ! -z "$included_patches" ]] && populate_patches "-i" "$included_patches"
 
-echo "************************************"
-echo "Building YouTube APK"
-echo "************************************"
-
 mkdir -p build
 
-if [ -f "com.google.android.youtube.apk" ]; then
-    echo "Building Root APK"
-    java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches.jar --mount \
-        -e microg-support ${patches[@]} \
-        $EXPERIMENTAL \
-        -a com.google.android.youtube.apk -o build/revanced-root.apk
-    echo "Building Non-root APK"
-    java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches.jar \
-        ${patches[@]} \
-        $EXPERIMENTAL \
-        -a com.google.android.youtube.apk -o build/revanced-nonroot.apk
-else
-    echo "Cannot find YouTube APK, skipping build"
-fi
-echo ""
-echo "************************************"
-echo "Building YouTube Music APK"
-echo "************************************"
-if [ -f "com.google.android.apps.youtube.music.apk" ]; then
-    echo "Building Root APK"
-    java -jar revanced-cli.jar -b revanced-patches.jar --mount \
-        -e microg-support ${patches[@]} \
-        $EXPERIMENTAL \
-        -a com.google.android.apps.youtube.music.apk -o build/revanced-music-root.apk
-    echo "Building Non-root APK"
-    java -jar revanced-cli.jar -b revanced-patches.jar \
-        ${patches[@]} \
-        $EXPERIMENTAL \
-        -a com.google.android.apps.youtube.music.apk -o build/revanced-music-nonroot.apk
-else
-    echo "Cannot find YouTube Music APK, skipping build"
-fi
+# List of arguments expected by ReVanced CLI
+readonly INTEGRATIONS=("-m revanced-integrations.apk")
+readonly MOUNT=("--mount" "-e microg-support")
+readonly YT=("-a com.google.android.youtube.apk" "-o build/revanced-")
+readonly YTM=("-a com.google.android.apps.youtube.music.apk" "-o build/revanced-music-")
+readonly ROOT_SUFFIX="root.apk"
+readonly NON_ROOT_SUFFIX="nonroot.apk"
+
+# Compile build based on user's preferences. If nothing is specified, it will build everything
+case "$APP-$VARIANT" in
+    "YT-"|"yt-")
+        echo "Building both variants of YouTube"
+        build_apps "${INTEGRATIONS[@]}" "${MOUNT[@]}" "${YT[@]}$ROOT_SUFFIX"
+        build_apps "${INTEGRATIONS[@]}" "" "" "${YT[@]}$NON_ROOT_SUFFIX"
+        ;;
+    "YT-ROOT"|"yt-root")
+        echo "Building root variant of YouTube"
+        build_apps "${INTEGRATIONS[@]}" "${MOUNT[@]}" "${YT[@]}$ROOT_SUFFIX"
+        ;;
+    "YT-NON-ROOT"|"yt-non-root")
+        echo "Building non-root variant of YouTube"
+        build_apps "${INTEGRATIONS[@]}" "" "" "${YT[@]}$NON_ROOT_SUFFIX"
+        ;;
+    "YTM-"|"ytm-")
+        echo "Building both variants of YouTube Music"
+        build_apps "" "${MOUNT[@]}" "${YTM[@]}$ROOT_SUFFIX"
+        build_apps "" "" "" "${YTM[@]}$NON_ROOT_SUFFIX"
+        ;;
+    "YTM-ROOT"|"ytm-root")
+        echo "Building root variant of YouTube Music"
+        build_apps "" "${MOUNT[@]}" "${YTM[@]}$ROOT_SUFFIX"
+        ;;
+    "YTM-NON-ROOT"|"ytm-non-root")
+        echo "Building non-root variant of YouTube Music"
+        build_apps "" "" "" "${YTM[@]}$NON_ROOT_SUFFIX"
+        ;;
+    "-ROOT"|"-root")
+        echo "Building ROOT variant of YT & YTM"
+        build_apps "${INTEGRATIONS[@]}" "${MOUNT[@]}" "${YT[@]}$ROOT_SUFFIX"
+        build_apps "" "${MOUNT[@]}" "${YTM[@]}$ROOT_SUFFIX"
+        ;;
+    "-NON-ROOT"|"-non-root")
+        echo "Building NON-ROOT variant of YT & YTM"
+        build_apps "${INTEGRATIONS[@]}" "" "" "${YT[@]}$NON_ROOT_SUFFIX"
+        build_apps "" "" "" "${YTM[@]}$NON_ROOT_SUFFIX"
+        ;;
+    *)
+        echo "Building Everything"
+        build_apps "${INTEGRATIONS[@]}" "${MOUNT[@]}" "${YT[@]}$ROOT_SUFFIX"
+        build_apps "${INTEGRATIONS[@]}" "" "" "${YT[@]}$NON_ROOT_SUFFIX"
+        build_apps "" "${MOUNT[@]}" "${YTM[@]}$ROOT_SUFFIX"
+        build_apps "" "" "" "${YTM[@]}$NON_ROOT_SUFFIX"
+        ;;
+esac
